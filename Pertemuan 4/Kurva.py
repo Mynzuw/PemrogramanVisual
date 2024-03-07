1549f5368ad9756daeae3da5d84d710224d4286c
Miftah Adha <110320264+Mynzuw@users.noreply.github.com>
2024-03-07 10:10:54 +0700

    Add files via upload

---
+++ Pertemuan 4/Kurva.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-4,4,10000)
+plt.figure(figsize=(3,30))
+
+#Gambar lingkaran kecil
+plt.plot(x, (0.01-x**2)**0.5,       '-k')
+plt.plot(x, -((0.01-x**2)**0.5),    '-k')
+
+#Tentukan persamaan matematika
+y = x -x -0
+y1 = x**3 - 2*x**2 - 5*x + 6
+
+plt.plot(x, y, '-k')
+
+plt.plot(x, y1, '-r', label='y1')
+
+plt.legend(loc='upper left')
+plt.grid()
+plt.show()
+
